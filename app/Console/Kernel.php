6fcf903ab8ce32321ff07c9b54670d7d3ddbf601
Violetta <savina_vs2000@mail.ru>
2024-02-26 00:18:18 +0500

    commit

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,11 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Events\SendApiRequest;
+use Illuminate\Support\Facades\Http;
+use \Datetime;
+use App\Models\Client;
+use App\Models\Message;
 
 class Kernel extends ConsoleKernel
 {
@@ -15,7 +20,43 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+
+            $clients = Client::all();
+            $messages = Message::all();
+            $phones1 = [];
+
+            function daysUntilBirthday($birthday) {
+                $now = new DateTime();
+                $birthday = DateTime::createFromFormat('Y-m-d', $birthday);
+                $birthday->setDate($now->format('Y'), $birthday->format('m'), $birthday->format('d'));
+
+                return $birthday->diff($now)->days;
+            }
+
+            function sendSMS($login, $psw, $phones, $mes) {
+                $url = "https://smsc.ru/sys/send.php?login={$login}&psw={$psw}&phones={$phones}&mes={$mes}";
+                return Http::post($url);
+            }
+
+            foreach ($clients as $client) {
+                $daysLeft = daysUntilBirthday($client->birthday);
+
+                if ($daysLeft >= 1 && $daysLeft <= 7) {
+                    $phones1[] = $client->phone;
+                }
+            }
+
+            $phones = implode(', ', $phones1);
+            $login = env('SMS_LOGIN');
+            $psw = env('SMS_PASSWORD');
+
+            foreach ($messages as $message) {
+                $mes = $message->text;
+                sendSMS($login, $psw, $phones, $mes);
+            }
+
+        })->dailyAt('19:13');
     }
 
     /**
--- app/Http/Controllers/Api/ClientController.php
+++ app/Http/Controllers/Api/ClientController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Carbon\Carbon;
 use \Datetime;
 use Illuminate\Support\Facades\Http;
+use App\Models\Message;
 
 class ClientController extends Controller
 {
@@ -19,56 +20,9 @@ class ClientController extends Controller
     public function index()
     {
         $clients = Client::all();
+        return $clients;
 
-        // function daysUntilBirthday($birthday) {
-        //     $now = new DateTime();
-        //     $birthday = DateTime::createFromFormat('Y-m-d', $birthday);
-        //     $birthday->setDate($now->format('Y'), $birthday->format('m'), $birthday->format('d'));
-
-        //     return $birthday->diff($now)->days;
-        // }
-
-        // $phones = [];
-
-        // foreach ($clients as $client) {
-        //     $daysLeft = daysUntilBirthday($client->birthday);
-
-        //     if ($daysLeft >= 1 && $daysLeft <= 7) {
-        //         $phones[] = $client->phone;
-        //     }
-        // }
-
-        // return $phones;
-        // return $clients;
-
-
-        // $response = Http::withHeaders([
-        //     'login' => 'Violetta123456',
-        //     'psw' => 'VG9K!6CV-6kK3Jt',
-        //     'phones' => '79519820329',
-        //     'mes' => 'тест1',
-        // ])->post('https://smsc.ru/sys/send.php');
-
-        // if ($response->ok()) {
-        //     $body = $response->json();
-        // }
-
-
-
-        function sendSMS($login, $psw, $phones, $mes) {
-            $url = "https://smsc.ru/sys/send.php?login={$login}&psw={$psw}&phones={$phones}&mes={$mes}";
-            return Http::post($url);
-        }
-
-        $login = "Violetta123456";
-        $psw = "VG9K!6CV-6kK3Jt";
-        $phones1 = ["79519820329","79199528228"];
-        $phones = implode(', ', $phones1);
-        $mes = "тест";
-
-        sendSMS($login, $psw, $phones, $mes);
-
-        // Http::post('https://smsc.ru/sys/send.php?login=Violetta123456&psw=VG9K!6CV-6kK3Jt&phones=79519820329&mes=тест');
+        //return now()->format('H:i');
     }
 
     /**
